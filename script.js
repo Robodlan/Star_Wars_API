0abaf1cec4072fd268984793ef006e91663df8d8
Robodlan <reinaldo.robles@icloud.com>
2024-03-26 19:40:26 -0400

    commit

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-import express from "express";
+import express, { json } from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
 const app = express()
 const port = 3000;
-const url = "https://swapi.dev/api/planets/"
+const url = "https://swapi.dev/api/planets/1/"
+
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
-
-
 app.get("/", async (req, res) => {
   try {
-    const response = await axios.get(url);
-    const result = response.data;
-    console.log(result)
-     res.render("index.ejs", { data: "Show Planet" });
+   res.render("index.ejs", { data: "Show Planet" });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -26,12 +22,10 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
   try {
-
-    const response = await axios.get(url);
+    const response = await axios.post(url);
+    console.log(req.body)
     const result = response.data;
-    
-    
-    res.render("index.ejs", { data: result });
+    res.render("index.ejs", { data: JSON.stringify(result) });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
